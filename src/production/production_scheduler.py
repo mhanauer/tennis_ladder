96dbff29ee84f9d092f06116a14ccaf1107bde65
Your Name <you@example.com>
2023-10-23 14:01:12 +0000

    Added bulk upload and hopefully avoided name problem

--- src/production/production_scheduler.py
+++ src/production/production_scheduler.py
@@ -21,25 +21,28 @@ def calculate_points(match_type, win_loss, score, challenger=None):
             if len(sets) > 2 or '0-1' in sets or '1-0' in sets:
                 return (3, 1)
             else:
-                return (3, 0)  # The Challenged gets 0 points if they lose in straight sets
+                return (3, 0)
                 
         elif win_loss == 'Loss':
             if len(sets) > 2 or '0-1' in sets or '1-0' in sets: 
                 return (1, 3)
             else:
-                # The Challenger gets -1 if they lose in straight sets,
-                # The Challenged gets 0
                 return (-1 if challenger == 'Challenger' else 0, 3)
                 
     return ('Invalid input', 'Invalid input')
 
-
-
-
 def main():
     st.title('Match Points Calculator')
-
-    # Load data from the previous session
+        # Bulk upload option
+    uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+    if uploaded_file is not None:
+        bulk_data = pd.read_csv(uploaded_file)
+        st.write("Uploaded data:")
+        st.table(bulk_data)
+        if st.button('Append Uploaded Data'):
+            # Assuming data is your main DataFrame
+            data = pd.concat([data, bulk_data]).reset_index(drop=True)
+    
     try:
         data = pd.read_csv('data.csv')
     except (FileNotFoundError, pd.errors.EmptyDataError):
@@ -64,6 +67,7 @@ def main():
         'Christian Basa',
         'Craig Radley',
         'Curt Lawson',
+        "Dan Nenon",
         'Dave Belcher',
         'Ed Sch',
         'Erik Swanson',
@@ -102,10 +106,8 @@ def main():
         'Youngjun Lee'
     ]
 
-
     name_me = st.selectbox('Select your name:', names_list)
     name_opponent = st.selectbox('Select your opponent:', names_list)
-
     match_type = st.selectbox('Select the match type:', ['Proposal Match', 'Challenge Match'])
     
     challenger = None
@@ -118,7 +120,6 @@ def main():
     if st.button('Calculate Points'):
         points_me, points_opponent = calculate_points(match_type, win_loss, score, challenger)
         
-        # Player's data
         data = data.append({
             'Name': name_me,
             'Opponent': name_opponent,
@@ -129,8 +130,6 @@ def main():
             'Points': points_me
         }, ignore_index=True)
 
-        # Opponent's data
-        # Opponent's data
         data = data.append({ 
             'Name': name_opponent,
             'Opponent': name_me,
@@ -140,26 +139,21 @@ def main():
             'Score': score,
             'Points': points_opponent
         }, ignore_index=True)
-
-
+        
     if not data.empty:
-        # Display the data table
         st.table(data)
 
-        # Option to delete a row
-        selected_row = st.selectbox('Select a row to delete (refresh brower tab to see changes):', range(len(data)), format_func=lambda x: f'Row {x}')
+        selected_row = st.selectbox('Select a row to delete (refresh browser tab to see changes):', range(len(data)), format_func=lambda x: f'Row {x}')
         if st.button('Delete selected row'):
             data = data.drop(selected_row).reset_index(drop=True)
-
-    # Save data for the next session
+    
     data.to_csv('data.csv', index=False)
 
-if not data.empty and 'Name' in data.columns:
-    total_points = data.groupby('Name')['Points'].sum().sort_values(ascending=False)
-    st.table(total_points)
-else:
-    st.write("No data available or 'Name' column missing.")
-
+    if not data.empty and 'Name' in data.columns:
+        total_points = data.groupby('Name')['Points'].sum().sort_values(ascending=False)
+        st.table(total_points)
+    else:
+        st.write("No data available or 'Name' column missing.")
 
 if __name__ == "__main__":
     main()
